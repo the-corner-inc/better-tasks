854ca5fe4e9f711fa95b8c4b099a3ca32efd03cb
elias <borrajo.elias@gmail.com>
2026-01-26 02:37:39 +0100

    feat: add a client-only counter. use of local storage

--- src/components/local-count-button.tsx
+++ src/components/local-count-button.tsx
@@ -1,5 +1,6 @@
 import {Button} from "@/components/ui/button.tsx"
 import {useEffect, useState} from "react";
+import {ClientOnly} from "@tanstack/react-router";
 
 
 // CLIENT ONLY CODE - STORE IN STORAGE
@@ -7,12 +8,18 @@ import {useEffect, useState} from "react";
 // So some components can be WRAPPED around some client-only components to be used more easily
 export function LocalCountButton() {
 
-    const [count, setCount] = useState(0);
+    return (
+        <ClientOnly>         {/* Client Only, is ONLY rendered on the client side */}
+            <ClientSection/>
+        </ClientOnly>
+        )
 
-    // Gets the data from storage to screen
-    useEffect(() => {
-        setCount(loadCount)
-    }, []);
+}
+
+// Wrapper Component for Client-Only section
+function ClientSection() {
+
+    const [count, setCount] = useState(loadCount);
 
     // Stores the data on screen to the storage
     useEffect(() => {
@@ -32,8 +39,6 @@ export function LocalCountButton() {
         </Button>
     )
 }
-
-
 
 // Loads the data from storage to UI data
 function loadCount() {

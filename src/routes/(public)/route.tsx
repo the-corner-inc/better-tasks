2674fd4bd9adf8fa46cea0173a2d1b270521ae46
elias <borrajo.elias@gmail.com>
2026-02-03 16:40:54 +0100

    fix(routes): add an outlet to the public route

--- src/routes/(public)/route.tsx
+++ src/routes/(public)/route.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {createFileRoute, Outlet} from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(public)')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/(public)"!</div>
+  return (
+      <>
+        <div>Hello "/(public)"!</div>
+        <main className="container py-8">
+          <Outlet/>
+        </main>
+      </>
+  )
 }
